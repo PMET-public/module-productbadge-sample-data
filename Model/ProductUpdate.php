94045fcde5f2d63c8f528966d86e4645f7370614
Jeff Britts <jbritts@adobe.com>
2023-01-26 07:37:33 -0600

    remove fastsimpleimport references

--- Model/ProductUpdate.php
+++ Model/ProductUpdate.php
@@ -47,7 +47,8 @@ class ProductUpdate
             foreach ($rows as $row) {
                 $_productsArray[] = array_combine($header, $row);
             }
-            $this->importerModel = $this->objectManager->create('FireGento\FastSimpleImport\Model\Importer');
+            $this->importerModel = $this->objectManager->create('MagentoEse\DataInstall\Model\Import\Importer\Importer');
+            $this->importerModel->setEntityCode('catalog_product');
             $this->importerModel->setValidationStrategy('validation-skip-errors');
             try {
                 $this->importerModel->processImport($_productsArray);
